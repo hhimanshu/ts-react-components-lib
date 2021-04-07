fb11fa2e15ab3f3ed044d9c25ef3ceb41d74d2dc
hhimanshu <harit.himanshu@yahoo.in>
2021-04-07 16:46:12 -0700

    (fix): Lint test

--- src/components/Button.test.tsx
+++ src/components/Button.test.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Button } from './Button';
 import { render, screen } from '@testing-library/react';
 
-
 describe('A Button', () => {
   it('should display button', () => {
     render(<Button label={'Click Me'}>Click Me</Button>);
